88ca6ed11023a6929c3728f2e33d708e743b3e5a
SheikhTrump <onlywebs3500@gmail.com>
2025-03-30 16:28:05 +0600

    Daily update

---
+++ market.py
@@ -0,0 +1,63 @@
+from flask import Flask , render_template
+import requests
+import json
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+
+db = SQLAlchemy(app)
+'''
+class Item(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name = db.Column(db.String(length=30),nullable=False,unique=True)
+    price=db.Column(db.Integer(),nullable=False)
+    barcode = db.Column(db.String(length=32),nullable=False,unique=True)
+    description = db.Column(db.String(length = 1024),nullable=False,unique = True)
+'''
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    
+
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+'''
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+
+@app.route('/about/<username>')
+def about(username):
+    return f'<h1>HEllo from ABOUT {username} </h1>'
+
+'''
+@app.route("/market")
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template("market.html",items=items)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
